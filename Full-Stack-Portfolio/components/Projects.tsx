1a4ce8755432d4099761298b91ac83fadbea6a31
Haider Ali <haideralihashmi2308@gmail.com>
2026-01-30 10:44:28 +0500

    Production Build v1.0

--- Full-Stack-Portfolio/components/Projects.tsx
+++ Full-Stack-Portfolio/components/Projects.tsx
@@ -7,7 +7,7 @@ const categories = ["All", "Frontend", "Backend", "Fullstack", "Mobile"];
 
 const projects = [
   {
-    title: "E-Commerce",
+    title: "E-Commerce Web App",
     description: "Complete shopping solution with Stripe, tracking, and admin panel.",
     image: "https://plus.unsplash.com/premium_photo-1664201890375-f8fa405cdb7d?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     tags: ["Next.js", "MongoDB", "Stripe"],
